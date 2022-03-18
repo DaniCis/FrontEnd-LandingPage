c94f10f60c143dbc7d1e0e5dbaa6931e45131108
DaniCis <danicisneros2@hotmail.com>
2022-03-17 20:40:04 -0500

    add btn to go up

---
+++ js/script.js
@@ -0,0 +1,16 @@
+btn = document.getElementById("boton-up");
+btn.addEventListener("click", scrollUp);
+function scrollUp(){
+    var currentScroll = document.documentElement.scrollTop;
+    if (currentScroll > 0){
+        window.requestAnimationFrame(scrollUp);
+        window.scrollTo (0, currentScroll - (currentScroll / 20));
+    }
+}
+window.onscroll = function(){
+    var scroll = document.documentElement.scrollTop;
+    if (scroll > 500)
+        btn.style.transform = "scale(1)";
+    else if(scroll < 500)
+        btn.style.transform = "scale(0)";
+}
